d336917e10d1c99c2b2e0cba6cf9a6a13415b530
Reiniel Ravelo <reinielravelo@gmail.com>
2019-03-12 07:25:28 -0400

    Added Events
    .add_btn (to add content when clicked) and
    keypress (to add content when Enter is pressed)

--- starter/app.js
+++ starter/app.js
@@ -1,24 +1,46 @@
+// BUDGET CONTROLLER 
 var budgetController = (function() {
-    
-    var x = 23; 
 
-    var add = function(a) {
-        return x + a;
-    }
+//Some cod
 
-    return {
-        publicTest: function(b) {
-            console.log(add(b));
-        }
-    }
 })(); 
 
-
-
+// UI CONTROLLER
 var UIController = (function() {
 
-    // Some code 
+// Some code 
 
 })(); 
 
-var controller = (fucntion)
+
+// GLOBAL APP CONTROLLER 
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+var ctrlAddItem = function() {
+
+
+// 1. Get the field input data
+
+// 2. Add the item to the controller 
+
+// 3. Add the item to the UI
+
+// 4. Calculate the budget 
+
+// 5. Display the budget on the UI 
+
+}
+
+document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+document.addEventListener('keypress', function(event){
+ 
+ if (event.keyCode === 13 || event.which === 13) {
+     ctrlAddItem(); 
+  }
+
+});
+
+
+})(budgetController, UIController);

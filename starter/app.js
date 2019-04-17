c720607af7ddbda475361b80f497acdc718314ac
Reiniel Ravelo <reinielravelo@gmail.com>
2019-04-17 18:50:24 -0400

    update

--- starter/app.js
+++ starter/app.js
@@ -174,6 +174,29 @@ var DOMstrings = {
   expensesPercLabel: '.item__percentage'
 };
 
+
+var formatNumber = function(num, type) {
+var numSplit, int, dec, type; 
+
+num = Math.abs(num);
+num = num.toFixed(2);
+
+numSplit = num.split('.'); 
+
+int = numSplit[0];
+
+if (int.length > 3) {
+    int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); 
+}
+
+dec = numSplit[1];
+
+type === 'exp' ?  sign = '-' : sign = '+';
+
+return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec; 
+
+};
+
 return {
     getInput: function() {
         return {
@@ -201,7 +224,7 @@ return {
 
     newHtml = html.replace('%id%', obj.id); 
     newHtml = newHtml.replace('%description%', obj.description); 
-    newHtml = newHtml.replace('%value%', obj.value);  
+    newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));  
 
     // 3. Insert the HTML into the DOM
     document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);   
@@ -230,9 +253,12 @@ el.parentNode.removeChild(el);
 }, 
 displayBudget: function(obj) {
 
-document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget; 
-document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc; 
-document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;  
+var type;
+obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type); 
+document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc'); 
+document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');  
 
 if (obj.percentage > 0) {
     document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + "%"; 
@@ -264,6 +290,7 @@ nodeListForEach(fields, function(current, index) {
 
 
 },
+
 
  getDOMstrings: function() {
       return DOMstrings; 
